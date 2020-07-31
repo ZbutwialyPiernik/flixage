4951258a30e5c1817846f679228c806d106ac6b3
ZbutwialyPiernik <23108582+ZbutwialyPiernik@users.noreply.github.com>
2020-07-31 21:28:25 +0200

    Fixed album controller

--- service-resource/src/main/java/com/zbutwialypiernik/flixage/controller/AlbumController.java
+++ service-resource/src/main/java/com/zbutwialypiernik/flixage/controller/AlbumController.java
@@ -7,8 +7,12 @@ import com.zbutwialypiernik.flixage.service.AlbumService;
 import com.zbutwialypiernik.flixage.service.QueryableService;
 import ma.glasnost.orika.MapperFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(("/albums"))
 public class AlbumController extends QueryableController<Album, AlbumResponse>  {
 
     public AlbumController(QueryableService<Album> service, MapperFactory mapperFactory) {
